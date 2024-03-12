4733a9f43847c900ad9d242e7383ec5a3b4ede54
Dougllas Sousa <cursodsousa@gmail.com>
2024-03-12 15:35:33 -0300

    [cadatro-clientes] Iniciando a camada de apresentacao

--- cadastro-clientes/src/io/github/cursodsousa/clientes/Main.java
+++ cadastro-clientes/src/io/github/cursodsousa/clientes/Main.java
@@ -1,10 +1,10 @@
 package io.github.cursodsousa.clientes;
 
-import io.github.cursodsousa.clientes.dominio.Cliente;
+import io.github.cursodsousa.clientes.apresentacao.TelaCadastro;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        System.out.println(cliente.getCodigo());
+        TelaCadastro telaCadastro = new TelaCadastro();
+        telaCadastro.setVisible(true);
     }
 }
---
+++ cadastro-clientes/src/io/github/cursodsousa/clientes/apresentacao/TelaCadastro.java
@@ -0,0 +1,23 @@
+package io.github.cursodsousa.clientes.apresentacao;
+
+import javax.swing.*;
+
+public class TelaCadastro extends JFrame {
+
+    private JLabel labelNome;
+    private JLabel labelCpf;
+    private JLabel labelSexo;
+
+    public TelaCadastro(){
+        construirTela();
+    }
+
+    private void construirTela(){
+        setSize(600,500);
+        setTitle("Cadastro de Cliente");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(null);
+
+
+    }
+}
