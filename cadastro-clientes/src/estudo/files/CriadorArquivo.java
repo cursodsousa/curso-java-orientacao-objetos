a415dbd1332fe6c509f1f0f61023cdbadd006829
Dougllas Sousa <cursodsousa@gmail.com>
2024-03-24 16:38:16 -0300

    [utilitarios] manipulacao arquivos em java

---
+++ cadastro-clientes/src/estudo/files/CriadorArquivo.java
@@ -0,0 +1,69 @@
+package estudo.files;
+
+import java.io.*;
+
+public class CriadorArquivo {
+    public static void main(String[] args) {
+        CriadorArquivo criadorArquivo = new CriadorArquivo();
+        criadorArquivo.lerArquivoTexto();
+    }
+
+    public void deletarArquivo(){
+        File arquivo = new File("C:\\sistemas\\arquivos\\arquivo.txt");
+        boolean deletou = arquivo.delete();
+        System.out.println("Deletou? " + deletou);
+    }
+
+    public void lerArquivo(){
+        File arquivo = new File("C:\\sistemas\\arquivos\\arquivo.txt");
+        System.out.println("Caminho: " + arquivo.getAbsolutePath());
+        System.out.println("Nome: " + arquivo.getName());
+        System.out.println("Tamanho: " + arquivo.length());
+        System.out.println("É arquivo?: " + arquivo.isFile());
+    }
+
+    public void criarPasta(){
+        File file = new File("C:\\sistemas\\arquivos\\pasta");
+        boolean criou = file.mkdir();
+        System.out.println("Pasta criada? " + criou);
+    }
+
+    public void lerArquivoTexto(){
+        try {
+            File arquivo = new File("C:\\sistemas\\arquivos\\arquivo.txt");
+
+            FileReader fileReader = new FileReader(arquivo);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ( (linha = reader.readLine()) != null ){
+                System.out.println(linha);
+            }
+
+            reader.close();
+
+        } catch (IOException e){
+
+        }
+
+    }
+
+    public void criarArquivo(){
+        try {
+            File arquivo = new File("C:\\sistemas\\arquivos\\arquivo.txt");
+
+            FileWriter fileWriter = new FileWriter(arquivo);
+
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+
+            writer.write("Conteudo do novo arquivo.");
+
+            writer.flush();
+            writer.close();
+
+        } catch (Exception e){
+            System.out.println("Ocorreu um erro: " + e.getMessage());
+        }
+    }
+}
